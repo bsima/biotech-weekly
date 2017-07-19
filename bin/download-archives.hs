78f2c54521d9d96b02f9d99eaa4d681e7d7ae00d
Ben Sima <bensima@gmail.com>
2017-07-18 19:59:05 -0700

    WIP new script to download archives
    So far: downloads the archives and parses them. Still need to fix the
    images and canonical links in each issue.
    
    Here it just prints out how many archives there are:
    
    ben@neb biotech-weekly : time bin/download-archives.hs
    90
    bin/download-archives.hs  0.59s user 0.17s system 2% cpu 32.318 total

---
+++ bin/download-archives.hs
@@ -0,0 +1,81 @@
+#!/usr/bin/env stack
+{-
+  stack
+  --resolver lts-8.23
+  script
+  --package directory
+  --package filepath
+  --package process
+  --package HTTP
+  --package tagsoup
+  --package flow
+-}
+
+
+import qualified Data.List as List
+import Data.Maybe (catMaybes)
+import Flow
+import qualified Network.HTTP as Http
+import Text.HTML.TagSoup
+
+
+main :: IO ()
+main = do
+  src <- openUrl (baseUri ++ "/archive")
+  let parsed = parseTags src
+  let hrefs = getIndexLinks parsed
+  archives <- mapM downloadIssue hrefs
+  putStrLn $ show $ length archives
+
+
+baseUri :: String
+baseUri =
+  "http://www.biotechweekly.com"
+
+
+outDir :: String
+outDir =
+  "content/archive"
+
+
+openUrl :: String -> IO String
+openUrl x =
+  Http.getResponseBody =<< Http.simpleHTTP (Http.getRequest x)
+
+
+safeHead :: [a] -> Maybe a
+safeHead ls =
+  case ls of
+    (x:_) -> Just x
+    []    -> Nothing
+
+
+getIndexLinks :: [Tag String] -> [String]
+getIndexLinks tags =
+  sections (~/= "<a>") tags
+  |> map (filter isTagOpen)
+  |> map safeHead
+  |> catMaybes
+  |> map (fromAttrib "href")
+  |> filter (\x -> (not (x == "")))
+  |> filter (List.isPrefixOf "/20")
+  |> map (baseUri ++)
+
+
+getIssueContent :: [Tag String] -> [Tag String]
+getIssueContent html =
+  html
+  |> dropWhile (~/= "<center>")
+  |> takeWhile (~/= "</center>")
+
+
+downloadIssue :: String -> IO [Tag String]
+downloadIssue url =
+  do src <- openUrl url
+     let content = getIssueContent <| parseTags src
+     return content
+
+
+getCanonicalUrl :: String -> String
+getCanonicalUrl _ =
+  undefined
